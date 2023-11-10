0c5dd1879b602694fc1263e6f80754672aacbba2
loweffort-alt <farfan_alexander@outlook.com>
2023-11-10 00:39:28 -0500

    Refactor: Improve Footer Links Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import "./App.css";
 import Afiliaciones from "./components/Afiliaciones";
+import FooterMenu from "./components/FooterMenu";
 import Partners from "./components/Partners";
 import Premios from "./components/Premios";
 import RedesSociales from "./components/RedesSociales";
-import FooterMenu from "./components/FooterMenu";
 
 function reveal() {
   var elementReveal = document.querySelectorAll(".reveal");
@@ -24,32 +24,13 @@ function reveal() {
 window.addEventListener("scroll", reveal);
 
 function App() {
-  const footerLinks = {
-    services: {
-      "Analítica Digital e Inteligencia Artificial": "/",
-      "Canales Digitales y Growth Hacking": "/",
-      "Consultoría CRM": "/",
-      "Diseño de Experiencia": "/",
-      "E-commerce": "/",
-      "Headhunting y Mentoring": "/",
-      "Marketing Automation": "/",
-      "Publicidad Digital": "/",
-    },
-    about: { "Sobre nosotros": "/", "Trabaja en Neo": "/", "Guía Bcorp": "/" },
-    countries: { Perú: "/", Colombia: "/", Chile: "/", México: "/" },
-    resources: { Blog: "/", Descargables: "/" },
-  };
-
   return (
     <>
       <section className="h-screen flex justify-center items-center font-bold text-3xl">
         <span>Scroll Down</span>
       </section>
       <section className="flex flex-col justify-between w-full min-[1200px]:flex-row reveal">
-        <FooterMenu links={footerLinks.services} name="Servicios" />
-        <FooterMenu links={footerLinks.about} name="Sobre NEO" />
-        <FooterMenu links={footerLinks.countries} name="Países" />
-        <FooterMenu links={footerLinks.resources} name="Recursos" />
+        <FooterMenu />
       </section>
       <section className="grid-container items-center pt-10">
         <RedesSociales />
--- src/components/FooterMenu.jsx
+++ src/components/FooterMenu.jsx
@@ -1,44 +1,28 @@
-import { useState } from "react";
-import { DownArrow, UpArrow } from "../assets/arrows";
+import FooterLinks from "./FooterLinks";
 
-const FooterMenu = ({ links, name }) => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const link = (e) => (
-    <li
-      key={e[0]}
-      className="pb-2 hover:cursor-pointer hover:underline min-[1200px]:pb-3"
-    >
-      <a href={e[1]}>{e[0]}</a>
-    </li>
-  );
-
+const FooterMenu = () => {
+  const footerLinks = {
+    services: {
+      "Analítica Digital e Inteligencia Artificial": "/",
+      "Canales Digitales y Growth Hacking": "/",
+      "Consultoría CRM": "/",
+      "Diseño de Experiencia": "/",
+      "E-commerce": "/",
+      "Headhunting y Mentoring": "/",
+      "Marketing Automation": "/",
+      "Publicidad Digital": "/",
+    },
+    about: { "Sobre nosotros": "/", "Trabaja en Neo": "/", "Guía Bcorp": "/" },
+    countries: { Perú: "/", Colombia: "/", Chile: "/", México: "/" },
+    resources: { Blog: "/", Descargables: "/" },
+  };
   return (
-    <div>
-      <div
-        className="flex justify-between py-3 border-b-[ccccd6] border-b-[1px] min-[1200px]:pb-3 min-[1200px]:border-0"
-        onClick={() => setIsOpen(!isOpen)}
-      >
-        <h3 className="font-bold text-[16px] leading-[24px] min-[1200px]:text-2xl">
-          {name}
-        </h3>
-        <div className="block min-[1200px]:hidden">
-          {!isOpen ? <UpArrow /> : <DownArrow />}
-        </div>
-      </div>
-      <div>
-        <ul className="hidden min-[1200px]:block leading-[20px] pt-1 font-medium">
-          {Object.entries(links).map((e) => link(e))}
-        </ul>
-        {isOpen && (
-          <>
-            <ul className="block min-[1200px]:hidden text-sm leading-[20px] pt-1">
-              {Object.entries(links).map((e) => link(e))}
-            </ul>
-          </>
-        )}
-      </div>
-    </div>
+    <>
+      <FooterLinks links={footerLinks.services} name="Servicios" />
+      <FooterLinks links={footerLinks.about} name="Sobre NEO" />
+      <FooterLinks links={footerLinks.countries} name="Países" />
+      <FooterLinks links={footerLinks.resources} name="Recursos" />
+    </>
   );
 };
 
